7e3fde70100b444f2ed9eaff1e4d66fc5132f6c4
brandofando <36392329+brandofando@users.noreply.github.com>
2018-03-24 18:22:03 -0400

    Assets

---
+++ Assets/_Scripts/GhostController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GhostController : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public Transform target;
+    public AudioSource ghostHit;
+    // Use this for initialization
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = target.position;
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("pacman"))
+        {
+            ghostHit.Play();
+        }
+    }
+}
---
+++ Assets/_Scripts/introScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class introScript : MonoBehaviour {
+
+    public Text instructions;
+    public float time = 8;
+    private float timeElapsed;
+
+    // Use this for initialization
+    void Start()
+    {
+        Destroy(instructions, time);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
